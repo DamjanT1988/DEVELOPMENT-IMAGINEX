fd4b58bcab9f1ffb015081474b33d7ede10938fe
DamjanT1988 <Damjan_t@hotmail.com>
2023-05-10 17:22:38 +0200

    2

--- config.php
+++ config.php
@@ -1,3 +1,4 @@
 <?php
-// API key
-define('API_KEY', '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');
+// API key 
+define('API_KEY', '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');
+//define('API_KEY', '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');
