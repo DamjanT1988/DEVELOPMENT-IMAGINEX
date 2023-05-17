416bf3d07334bae3dc805a6eab6bd424f21f3047
DamjanT1988 <Damjan_t@hotmail.com>
2023-05-18 01:06:33 +0200

    API KEY

--- config.php
+++ config.php
@@ -1,4 +1,3 @@
 <?php
 // API key 
-define('API_KEY', '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');
-//define('API_KEY', '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');
+define('API_KEY', 'PUT API KEY HERE');
